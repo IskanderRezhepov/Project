8eea82a29ce4070cec8f809b928b03e4fd28624f
IskanderRezhepov <iskarezh@gmail.com>
2025-04-16 09:13:12 +0000

    made the first plan of the project

---
+++ project.py
@@ -0,0 +1,18 @@
+#task management system
+from datetime import datetime
+
+tasks = []
+
+#1) create task
+
+#2) do something to the task
+
+#3) listing the tasks
+
+#4) sorting the tasks
+
+#5) truth table check 
+
+#6) search
+
+#7) menu
