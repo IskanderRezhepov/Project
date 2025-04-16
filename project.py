56c5183a5d1e39e5ebd18d837e74c9ee45f0e2f5
IskanderRezhepov <iskarezh@gmail.com>
2025-04-16 09:22:29 +0000

    made a start for the create_task code for sorting the tasks, started with adding the basic function of adding tasks to the task list

--- project.py
+++ project.py
@@ -1,9 +1,24 @@
 #task management system
 from datetime import datetime
 
+def create_task(title, description, due_date, priority):
+    return {'title': title, 'description': description, 'due_date': due_date, "priority": priority, 'completed': False}
+
 tasks = []
 
+task_dict = {}
+
 #1) create task
+
+def add_task():
+    title = input('Enter task title: ')
+    description = input("Enter task description: ")
+    due_date = input('Enter task due date: ') 
+    priority = int(input('Enter task priority (1-High, 2-Medium, 3-Low): '))
+    task = create_task(title, description, due_date, priority)
+    task.append(tasks)
+    task_dict[title] = task
+    print('Task has been successfully added')
 
 #2) do something to the task
 
