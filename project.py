c01b3e2b056201a2f2b4449b3298cc29e2bf6a90
IskanderRezhepov <iskarezh@gmail.com>
2025-04-17 11:34:15 +0000

    added task completion and viewing tasks functions. from first tests the addition of tasks works good and the code itself should be correct. final test would hopefully come with the set up of the menu

--- project.py
+++ project.py
@@ -2,7 +2,7 @@
 from datetime import datetime
 
 def create_task(title, description, due_date, priority):
-    return {'title': title, 'description': description, 'due_date': datetime.strptime(due_date, "%Y-%m-%d"), "priority": priority, 'completed': False}
+    return {'title': title, 'description': description, 'due_date': datetime.strptime(due_date, "%d.%m.%Y"), "priority": priority, 'completed': False}
 
 tasks = []
 
@@ -16,7 +16,7 @@ def add_task():
     due_date = input('Enter task due date: ') 
     priority = int(input('Enter task priority (1-High, 2-Medium, 3-Low): '))
     task = create_task(title, description, due_date, priority)
-    task.append(tasks)
+    tasks.append(task)
     task_dict[title] = task
     print('Task has been successfully added')
 
@@ -30,13 +30,46 @@ def delete_task():
         print('Task is not in the list')
 
 
-#2) do something to the task
+#2) do something to the task + truth table check 
+def mark_task_completed():
+    title = input("Enter the title of the completed task: ")
+    if title in task_dict:
+        task_dict[title]['completed'] = True
+        print(f"Task '{title}' marked as completed.")
+    else:
+        print("Task not found.")
+
+
+def view_all_tasks():
+    if not tasks:
+        print("No tasks available.")
+        return
+    for task in tasks:
+        if task['completed']:
+            status = '✔'
+        elif not task['completed']:
+            status = '✘'
+        print(f"[{status}] {task['title']} - Priority: {task['priority']}, Due: {task['due_date'].date()}")
+
+def view_pending_tasks():
+    pending_tasks = [task for task in tasks if not task['completed']]
+    if not pending_tasks:
+        print("No pending tasks.")
+        return
+    for task in pending_tasks:
+        if task['completed']:
+            status = '✔'
+        elif not task['completed']:
+            status = '✘'
+        print(f"[{status}] {task['title']} - Priority: {task['priority']}, Due: {task['due_date'].date()}")
+
+
+add_task()
+view_all_tasks()
 
 #3) listing the tasks
 
 #4) sorting the tasks
-
-#5) truth table check 
 
 #6) search
 
